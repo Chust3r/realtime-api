fd4ba1be0ab9a81d22aceb63cce2d5debc14838e
Chust3r <kriptoortega@gmail.com>
2024-10-02 15:26:19 -0600

    Setup entry app

--- index.ts
+++ index.ts
@@ -1,17 +1,20 @@
-import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serve } from '@hono/node-server'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
 import { routes } from '~routes'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-	return c.text('Hello Hono!')
-})
+app.use('*', cors())
+
+app.use(logger())
 
 app.route('/', routes)
 
 const port = 3000
-console.log(`Server is running on port ${port}`)
+
+console.log(`Server is running on port http://localhost:${port}`)
 
 serve({
 	fetch: app.fetch,
