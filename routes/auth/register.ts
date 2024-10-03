f6a2ca195d0397566681362d4119125ae1561cec
Chust3r <kriptoortega@gmail.com>
2024-10-03 17:58:32 -0600

    Update Register Route

--- routes/auth/register.ts
+++ routes/auth/register.ts
@@ -2,13 +2,14 @@ import { Hono } from 'hono'
 import { validator } from '~services/validator'
 import { user } from '~services/user'
 import { jwt } from '~services/jwt'
+import { setCookie } from 'hono/cookie'
 
 export const register = new Hono()
 
 register.post('/register', async (c) => {
 	const { req, json } = c
 	try {
-		const body = await req.json()
+		const body = (await req.json()) || {}
 
 		const { isValid, errors, data } = validator.validate('register', body)
 
@@ -30,7 +31,16 @@ register.post('/register', async (c) => {
 
 		const refreshToken = await jwt.sign('refresh', newUser)
 
-		return json({ ok: true, token, refreshToken }, 201)
+		//→ SET COOKIES
+
+		setCookie(c, 'token', token, {
+			httpOnly: true,
+		})
+		setCookie(c, 'refresh_token', refreshToken, {
+			httpOnly: true,
+		})
+
+		return json({ ok: true }, 201)
 	} catch (e) {
 		return json({ errors: ['Internal Server Error'] }, 500)
 	}
