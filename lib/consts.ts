5fc631ead25b05442e04457680299a4302a7ec52
Chust3r <kriptoortega@gmail.com>
2024-10-05 17:12:26 -0600

    Update JWT Time Expires

--- lib/consts.ts
+++ lib/consts.ts
@@ -4,6 +4,6 @@ export const DATABASE_URL = process.env.DATABASE_URL as string
 
 export const JWT_SECRET = process.env.JWT_SECRET || 'secret'
 
-export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1d'
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '6h'
 
-export const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '7d'
+export const JWT_REFRESH_EXPIRES_IN = process.env.JWT_REFRESH_EXPIRES_IN || '1d'
